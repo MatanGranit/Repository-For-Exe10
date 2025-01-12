04917910332f89107e11191495091b8dc61b6579
username <email.com>
2025-01-12 05:01:39 +0200

    Add initial HTML, CSS, and React files for the application

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Repository-For-Exe10
+https://github.com/MatanGranit/Repository-For-Exe10
---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Foo from "./Foo";
+
+function App() {
+    return (
+        <div>
+            <Foo />
+            <Foo />
+            <Foo />
+        </div>
+    );
+}
+
+export default App;
